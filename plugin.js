994543f9c0a35ab6e2abcf44bf85b52a003d7881
albertosouza <alberto.souza.99@gmail.com>
2019-07-02 02:15:08 -0300

    add plugin.js file and configs

---
+++ plugin.js
@@ -0,0 +1,22 @@
+/**
+ * Plugin.js file, set configs, routes, hooks and events here
+ *
+ * see http://wejs.org/docs/we/plugin
+ */
+
+const path = require('path');
+
+module.exports = function loadPlugin(projectPath, Plugin) {
+  const plugin = new Plugin(__dirname);
+
+  plugin.fastLoader = function fastLoader(we, done) {
+    done();
+  };
+
+  plugin.events.on('we:after:load:express', (we)=> {
+    const adminFiles = path.join( __dirname, 'prod');
+    we.express.use('/admin', we.utils.express.static(adminFiles));
+  });
+
+  return plugin;
+};
